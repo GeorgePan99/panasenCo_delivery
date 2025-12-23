3dc81fcec00ff83147d00c7106550880fdcf16d9
George Panasenko <georgy.panasenko@gmail.com>
2025-12-23 23:14:53 +0700

    Почему-то не ругается при создании Error

--- Result/AppError.cs
+++ Result/AppError.cs
@@ -4,10 +4,12 @@ public class AppError
 {
     public string Code { get; }
     public string Message { get; }
+    public AppErrorType Type { get; }
 
-    public AppError(string code, string message)
+    public AppError(string code, string message, AppErrorType type)
     {
         Code = code;
         Message = message;
+        Type = type;
     }
 }
---
+++ Result/AppErrorType.cs
@@ -0,0 +1,11 @@
+namespace Result;
+
+public enum AppErrorType
+{
+    Validation,
+    NotFound,
+    Conflict,
+    Unauthorized,
+    Forbidden,
+    Unexpected
+}
